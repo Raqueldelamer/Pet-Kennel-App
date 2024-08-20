f11533dfb73f83329692af468ef2263e74dbcd89
Raquel Marie <raqueldelamer@gmail.com>
2024-08-20 12:04:16 -0500

    update js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let state = {
     email: "",
     city: "",
     zipCode: 0,
-    petsName: ""
+    petName: ""
 
 };
 
@@ -15,21 +15,34 @@ let saveBtn = document.getElementById("saveButton");
 let firsNameInput = document.getElementById("fname");
 let lastNameInput = document.getElementById("lname");
 let phoneNumberInput = document.getElementById("phone-number");
-let city = document.getElementById("city");
+let emailInput = document.getElementById("email");
+let cityInput = document.getElementById("city");
 let zipCodeInput = document.getElementById("zipcode");
 let petNameInput = document.getElementById("pet-name");
 
 function handleSave() {
-    console.log("save button when clicked");
-    let name = nameInput.value;
-    state.name = name;
+    console.log("Save button when clicked");
+    let firstName = firstNameInput;
+    state.firstName = firstName;
+    
+    state.lastName = lastNameInput;
+    state.phoneNumber = phoneNumberInput;
+    state.email = emailInput;
+    state.city = cityInput;
+    state.zipCode = zipCodeInput;
+    state.petName = petNameInput;
+
     
     console.log("Added name" + state.name);
-
-    state.birthday = birthdayInput.value;
-    state.zipCode = zipCodeInput.value;
-
     console.log(state);
+    firstNameInput.value = "",
+    lastNameInput.value = "",
+    phoneNumberInput.value = "",
+    emailInput.value = "",
+    cityInput.value = "",
+    zipCodeInput.value = "",
+    petNameInput.value = ""
 
 }
+
 saveBtn.addEventListener("click", handleSave);
